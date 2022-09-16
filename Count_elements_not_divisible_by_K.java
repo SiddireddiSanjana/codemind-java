32df9bb354781655c1917b3bff9dcd8f8d3de832
SiddireddiSanjana <112751458+SiddireddiSanjana@users.noreply.github.com>
2022-09-16 16:04:08 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i,k,count=0;         n=sc.nextInt();         x=new int[n];         k=sc.nextInt();         for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]%k!=0)             count++;         }         System.out.println(count);     } }

--- Count_elements_not_divisible_by_K.java
+++ Count_elements_not_divisible_by_K.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
-class sample
+class Sample
 {
-public static void main(String args[])
-{
-    int x[],i,n,k,count=0;
-Scanner sc=new Scanner(System.in);
-
-n=sc.nextInt();
-x=new int[n];
-k=sc.nextInt();
-for(i=0;i<n;i++)
-x[i]=sc.nextInt();
-for(i=0;i<n;i++)
-{
-if(x[i]%k!=0)
-count++;
-}
-System.out.print(count);
-}
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,i,k,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%k!=0)
+            count++;
+        }
+        System.out.println(count);
+    }
 }
