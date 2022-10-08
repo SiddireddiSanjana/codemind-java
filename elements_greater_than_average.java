8a387d666db3d52a511a701c1341e2dc24a4f101
SiddireddiSanjana <112751458+SiddireddiSanjana@users.noreply.github.com>
2022-10-08 14:14:01 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);
            int n,x[],i,A,sum=0,c=0;
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
               {
                 x[i]=sc.nextInt();  
               } 
          for(i=0;i<n;i++)
          {
              sum=sum+x[i];
          }
          A=sum/n;
          for(i=0;i<n;i++)
          {
              if(x[i]>=A)
               c++;
          }
          System.out.println(c);
        }
    }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        int n,x[],i,A,sum=0,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+           {
+             x[i]=sc.nextInt();  
+           } 
+      for(i=0;i<n;i++)
+      {
+          sum=sum+x[i];
+      }
+      A=sum/n;
+      for(i=0;i<n;i++)
+      {
+          if(x[i]>=A)
+           c++;
+      }
+      System.out.println(c);
+    }
+}
